4b7c1590153af91ac12989b843dfbacd2438f7f6
agork <asimar100@gmail.com>
2018-05-08 09:39:09 +0200

    gallery work IP

---
+++ gallery.js
@@ -0,0 +1,12 @@
+var folder = "images/2018_03_15-16_sesja_ix/";
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) {
+                $("body").append( "<img src='"+ folder + val +"'>" );
+            }
+        });
+    }
+});
